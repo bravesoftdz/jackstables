eb79fb3b3a1c40979b9991c5890b23473d390d94
Jim Kinsman <jakinsman@gmail.com>
2016-06-04 10:46:24 -0500

    add base_url to fix on other sites

--- ci/application/views/admin_header.php
+++ ci/application/views/admin_header.php
@@ -5,7 +5,7 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?=$title?></title>
-        <link rel="stylesheet" href="/css/admin/styles.css">
+        <link rel="stylesheet" href="<?=base_url('css/admin/styles.css')?>">
 
         <!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
@@ -13,7 +13,7 @@
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
-	    <script src="/js/jquery-1.12.4.min.js"></script>
+	    <script src="<?=base_url('js/jquery-1.12.4.min.js')?>"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
